8663024f21dfcfbc25a2e3f900f21762ee2c9e55
amazinglzy <1075749478@qq.com>
2020-04-08 10:12:34 +0800

    support scan for TokenIterator

--- json-path/src/main/java/com/jayway/jsonpath/internal/eval/nav/iter/TokenIterator.java
+++ json-path/src/main/java/com/jayway/jsonpath/internal/eval/nav/iter/TokenIterator.java
@@ -11,6 +11,7 @@ public abstract class TokenIterator implements ResultIterator {
     protected ResultIterator parIter;
     protected PathToken token;
     protected IndexContext indexContext;
+    protected boolean isScan;
 
     protected NodeIterator currentNodeIter;
 
@@ -18,6 +19,15 @@ public abstract class TokenIterator implements ResultIterator {
         this.parIter = parIter;
         this.token = token;
         this.indexContext = indexContext;
+        this.isScan = false;
+        this.initCurrentNodeIter();
+    }
+
+    public TokenIterator(ResultIterator parIter, PathToken token, IndexContext indexContext, boolean isScan) {
+        this.parIter = parIter;
+        this.token = token;
+        this.indexContext = indexContext;
+        this.isScan = isScan;
         this.initCurrentNodeIter();
     }
 
@@ -38,9 +48,11 @@ public abstract class TokenIterator implements ResultIterator {
 
             while (this.currentNodeIter.hasNext()) {
                 if (this.parIter.getNode().getFirstVisit() < this.currentNodeIter.read().getFirstVisit()
-                        && this.currentNodeIter.read().getLastVisit() < this.parIter.getNode().getLastVisit()
-                        && this.currentNodeIter.read().getLevel() == this.parIter.getNode().getLevel() + 1) {
-                    return true;
+                        && this.currentNodeIter.read().getLastVisit() < this.parIter.getNode().getLastVisit()) {
+                    if (this.isScan || (this.parIter.getNode().getLevel() + 1 == this.currentNodeIter.read().getLevel()))
+                        return true;
+                    else
+                        this.currentNodeIter.next();
                 } else if (this.parIter.getNode().getLastVisit() < this.currentNodeIter.read().getFirstVisit()) {
                     break;
                 } else {
